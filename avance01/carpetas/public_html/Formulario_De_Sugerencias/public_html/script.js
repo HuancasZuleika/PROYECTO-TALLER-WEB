afc49c2631e7901eb7a8ca9d928feb94962b49ee
evelin-1404 <evelinml397@gmail.com>
2026-05-25 14:43:52 -0500

    Agregué formulario de sugerencias

--- avance01/carpetas/public_html/Formulario_De_Sugerencias/public_html/script.js
+++ avance01/carpetas/public_html/Formulario_De_Sugerencias/public_html/script.js
@@ -3,16 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 document.getElementById("formSugerencia").addEventListener("submit", function(e) {
+
     e.preventDefault();
 
     let nombre = document.getElementById("nombre").value;
+
     let email = document.getElementById("email").value;
+
     let mensaje = document.getElementById("mensaje").value;
 
     if(nombre === "" || email === "" || mensaje === "") {
+
         alert("Completa todos los campos");
+
     } else {
-        document.getElementById("respuesta").innerText = "¡Gracias por tu sugerencia!";
+
+        document.getElementById("respuesta").innerText =
+        "¡Gracias por tu sugerencia!";
+
         this.reset();
     }
 });
